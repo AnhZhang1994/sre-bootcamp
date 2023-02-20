0cffc972ed0d1d416e4839b765de246a95ab503e
Lu Zhang <50227257+AnhZhang1994@users.noreply.github.com>
2023-02-20 21:26:47 +0900

    Update tests.py

--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -1,18 +1,53 @@
 import unittest
 from methods import Token, Restricted
+from api import app
+import json
 
-
-class TestStringMethods(unittest.TestCase):
+class TestTokenMethods(unittest.TestCase):
 
     def setUp(self):
-        self.convert = Token()
-        self.validate = Restricted()
+        self.token = Token()
 
-    def test_generate_token(self):
-        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
+    def test_generate_token_valid(self):
+        # Test a valid login request
+        self.assertEqual(self.token.generate_token('admin', 'secret'), 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInJvbGUiOiJhZG1pbiJ9.bWUzyXuV7BpUTbL00D7anm_2GmV0rJ_Yzkg0fJ1C_bM')
 
-    def test_access_data(self):
-        self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
+    def test_generate_token_invalid(self):
+        # Test an invalid login request
+        with self.assertRaises(ValueError):
+            self.token.generate_token('admin', 'invalid')
+
+    def test_generate_token_missing_username(self):
+        # Test a missing username in login request
+        with self.assertRaises(TypeError):
+            self.token.generate_token(password='secret')
+
+    def test_generate_token_missing_password(self):
+        # Test a missing password in login request
+        with self.assertRaises(TypeError):
+            self.token.generate_token(username='admin')
+
+
+class TestRestrictedMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.restricted = Restricted()
+
+    def test_access_data_valid(self):
+        # Test a valid JWT token
+        auth_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInJvbGUiOiJhZG1pbiJ9.bWUzyXuV7BpUTbL00D7anm_2GmV0rJ_Yzkg0fJ1C_bM'
+        self.assertEqual(self.restricted.access_data(auth_token), 'You are under protected data')
+
+    def test_access_data_missing_token(self):
+        # Test missing JWT token
+        with self.assertRaises(ValueError):
+            self.restricted.access_data(None)
+
+    def test_access_data_invalid_token(self):
+        # Test an invalid JWT token
+        with self.assertRaises(ValueError):
+            self.restricted.access_data('Bearer invalid_token')
+
 
 if __name__ == '__main__':
     unittest.main()
